b0ff55df62fed876b02c932d74f3e89002b96c62
Danial <danazad2016@gmail.com>
2026-07-12 14:12:36 -0500

    Add retrieval for pushes

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from urllib.request import urlopen
-import json
 import sys
+import json
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
-while True:
-    try:
-        print('input username or quit')
-        username = input('>>> ')
+try:
+    user_input = sys.argv[1]
+    with urlopen(f'https://api.github.com/users/{user_input}/events') as response:
+        pushes = []
+        username = ''
 
-        if username == 'quit':
-            break
+        for entry in json.loads(response.read()):
+            if entry['type'] == 'PushEvent':
+                pushes.append(entry)
 
-        with urlopen(f'https://api.github.com/users/{username}/events') as response:
-            print(json.loads(response.read()))
-    except:
-        print('an expection has occurred.')
+        print(f'{user_input} has {len(pushes)} pushes')
+except IndexError:
+    print('ERROR: enter a username next to the filename of the program when running it.')
+except HTTPError as exception:
+    print(exception)
+except URLError as exception:
+    print(exception)
