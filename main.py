e7b52a96381e16ff032135050b5aa813888368cb
Danial <danazad2016@gmail.com>
2026-08-02 21:10:27 -0500

    perform a clean up

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sys
 import json
 from urllib.request import urlopen
-from urllib.error import URLError, HTTPError
 
 class Main:
     def search(self):
         try:
             self.username = sys.argv[1]
         except IndexError:
-            self.username = input('enter username: ')
+            self.username = input('enter the username: ')
 
         try:
             with urlopen(f'https://api.github.com/users/{self.username}/events') as response:
